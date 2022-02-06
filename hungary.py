df7a91bf075280abbbbb00d7843e3f601f57f6be
tachyon <nsbaghel81@gmail.com>
2022-02-06 20:23:46 +0530

    Thirsty plan added

--- hungary.py
+++ hungary.py
@@ -1,2 +1,10 @@
 hungry = input("Enter your input: ")
 print("Hungry is : ",hungry)
+if(hungry == "pizza"):
+    print("Not available")
+else:
+    thirsty = input("Are you thirsty?")
+    if(thirsty == "YES"):
+        print("Ok")
+    else:
+        print("Nice")
