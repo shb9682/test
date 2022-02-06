8599b8690c9050928ae1ff2bc27cfa99ef3beaae
tachyon <nsbaghel81@gmail.com>
2022-02-06 20:36:50 +0530

    4th commit on main

--- hungary.py
+++ hungary.py
@@ -2,6 +2,7 @@ hungry = input("Enter your input: ")
 print("Hungry is : ",hungry)
 if(hungry == "pizza"):
     print("Not available")
+    print("Hemlu")
 else:
     thirsty = input("Are you thirsty?")
     if(thirsty == "YES"):
